bbe640d6eb05c3c81dd4df0f7856666a37ecf292
byeonheejae <pion0458@gmail.com>
2025-04-08 10:14:02 +0900

    add Main3 solution2

--- src/main/java/Main3.java
+++ src/main/java/Main3.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main3 {
@@ -25,23 +27,30 @@ public class Main3 {
    */
 
   public String solution(String str) {
-    String answer = "";
-    int m = Integer.MIN_VALUE;
-    String[] s = str.split(" ");
-    for (String x : s) {
-      int length = x.length();
-      if(length > m ) {
-        m = length;
-        answer = x;
+    String answer = ""; // 가장 긴 단어를 저장할 변수
+    int m = Integer.MIN_VALUE; // 가장 긴 단어의 길이를 저장할 변수
+    String[] s = str.split(" "); // 문장을 공백 기준으로 나누어 배열 생성
+    for (String x : s) { // 배열의 각 단어를 순회
+      int length = x.length(); // 단어의 길이 검사
+      if(length > m ) { // 길이가 길면 갱신
+        m = length; // 최대 길이를 업데이트
+        answer = x; // 가장 긴 단어를 현재 단어로 업데이트
       }
     }
-    return answer;
+    return answer; // 가장 긴 단어 반환
+  }
+
+  // 스트림을 활용한 풀이
+  public String solution2(String str) {
+    return Arrays.stream(str.split(" "))
+        .max(Comparator.comparingInt(String::length))
+        .orElse("");
   }
 
   public static void main(String[] args) {
     Main3 T = new Main3();
     Scanner kb = new Scanner(System.in);
     String str = kb.nextLine();
-    System.out.println(T.solution(str));
+    System.out.println(T.solution2(str));
   }
 }
