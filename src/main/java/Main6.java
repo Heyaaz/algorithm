d979cf9b502765b99060612bac0eaf3cb980d7ff
byeonheejae <pion0458@gmail.com>
2025-04-08 15:09:18 +0900

    6. 중복 문자 제거 - 스트림

--- src/main/java/Main6.java
+++ src/main/java/Main6.java
@@ -1,9 +1,7 @@
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import javax.print.DocFlavor.STRING;
 
 /**
  * 설명
@@ -32,7 +30,6 @@ public class Main6 {
   public String solution(String str) {
     String answer = "";
     for (int i = 0; i < str.length(); i++) {
-//      System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
       if (str.indexOf(str.charAt(i)) == i) { // 현재 문자와 처음 등장하는 인덱스가 같으면
         answer += str.charAt(i); // answer에 추가
       }
@@ -41,11 +38,13 @@ public class Main6 {
   }
 
   // 스트림을 사용한 풀이
-//  public String solution2(String str) {
-//    return Arrays.stream(str)
-//        .distinct() // 중복된 문자 제거
-//        .collect(Collectors.toCollection(ArrayList::new)); // ArrayList로 변환
-//  }
+  public String solution2(String str) {
+    return str.chars() // 문자열을 int 스트림으로 변환
+        .mapToObj(c -> (char) c) // int 를 문자로 변환
+        .distinct() // 중복 제거
+        .map(String::valueOf) // 각 문자를 문자열로 변환
+        .collect(Collectors.joining()); // 문자열로 결합
+  }
 
   public static void main(String[] args) {
     Main6 T = new Main6();
