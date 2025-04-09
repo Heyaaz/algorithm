5d4d65bf7b112b31005f6c615f26dbe55107ea55
byeonheejae <pion0458@gmail.com>
2025-04-09 17:33:00 +0900

    8. 유효한 팰린드롬

--- src/main/java/팰린드롬.java
+++ src/main/java/팰린드롬.java
@@ -29,16 +29,21 @@ import java.util.Scanner;
 
 public class 팰린드롬 {
 
-  public String solutuion(String str) {
-
+  public String solution(String s) {
+    String answer = "YES";
+    s = s.toUpperCase().replaceAll("[^A-Z]", ""); // 알파벳 이외의 문자 제거, 대문자로 변환
+    String tmp = new StringBuilder(s).reverse().toString(); // 문자열을 뒤집어서 tmp에 저장
+    if (!s.equals(tmp)) { // 전과 후를 비교
+      answer = "NO";
+    }
+    return answer;
   }
-
 
   public static void main(String[] args) {
     팰린드롬 T = new 팰린드롬();
     Scanner kb = new Scanner(System.in);
     String str = kb.nextLine();
-    System.out.println(T.solutuion(str));
+    System.out.println(T.solution(str));
   }
 
 
